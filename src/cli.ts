a1c0719ed6ba6658143659b5661215bfa3177e64
Denis Sokolov <denis@sokolov.cc>
2025-05-25 12:00:00 +0000

    fix: better error stacktraces

--- src/cli.ts
+++ src/cli.ts
@@ -27,22 +27,16 @@ const exit = (code: number) => {
   realExit(code)
 }
 
-try {
-  const result = scanDirectoryWithResult(directory)
-  if (result.haveCycle) {
-    error(`Circular dependency existed in ${directory}`.red)
-    forEach(result.cycleList, (cycle, index) => {
-      error(`\ncycle ${index + 1}, size (${cycle.length}):${cycle.length === 1 ? " this file import itself".grey : " these files circular import each other".cyan}\n`)
-      // @ts-ignore
-      forEach(mapAbsPathsToRelPaths(cycle), c => error(`  ${c}`.red))
-    })
-    exit(1)
-  } else {
-    log(`Not found circular dependency in ${directory}`.green)
-    exit(0)
-  }
-} catch (error) {
-  log(error.message.red)
+const result = scanDirectoryWithResult(directory)
+if (result.haveCycle) {
+  error(`Circular dependency existed in ${directory}`.red)
+  forEach(result.cycleList, (cycle, index) => {
+    error(`\ncycle ${index + 1}, size (${cycle.length}):${cycle.length === 1 ? " this file import itself".grey : " these files circular import each other".cyan}\n`)
+    // @ts-ignore
+    forEach(mapAbsPathsToRelPaths(cycle), c => error(`  ${c}`.red))
+  })
   exit(1)
+} else {
+  log(`Not found circular dependency in ${directory}`.green)
+  exit(0)
 }
-
